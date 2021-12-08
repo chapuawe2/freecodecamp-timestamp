8eab9b25dc6680d13db759bfc5df8eaf6ab1bc08
Joshua Rodriguez <chapulinawebado@gmail.com>
2021-12-08 15:11:24 -0600

    Do what I did on replit

--- server.js
+++ server.js
@@ -1,32 +1,47 @@
-// server.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-var express = require('express');
-var app = express();
+//Replace env port variable by this
+const port = 443;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
-var cors = require('cors');
+const cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api/', function (req, res){
+  const date = new Date();
+  res.json({
+    unix: date.getTime(),
+    utc: date.toGMTString()
+  })
+})
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date", function (req, res) {
+  let date = req.params.date;
+
+  if ((/^\d+$/).test(date)){
+    date = parseInt(date);
+  }
+
+  date = new Date(date)
+  const response = {}
+
+  if (date == "Invalid Date"){
+    response.error = date.toString();
+  } else {
+    response.unix = date.getTime();
+    response.utc = date.toGMTString();
+  }
+
+  res.json(response);
 });
 
-
-
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+app.listen(process.env.PORT, ()=>{
+  console.log(`Listening on ${process.env.PORT}`);
 });
